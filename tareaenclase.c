069bbbd31ae1612313380c1042fa8582ae72574c
Alejandro-C12 <alejandro.cueva@udla.edu.ec>
2026-05-07 15:06:35 -0500

    5to commit: codigo pulido

--- tareaenclase.c
+++ tareaenclase.c
@@ -11,9 +11,9 @@ int main(int argc, char const *argv[]){
     printf("Ingrese 2do numero: ");
     scanf("%d", &b);
     printf("Resultado de la suma es: %d\n", sumar(a,b));
-    printf("Resultado de la resta es: %d ", restar(a,b));
-    printf("Resultado de la multiplicacion es: %d ", multiplicar(a,b));
-    printf("Resultado de la division es: %d ", division(a,b));
+    printf("Resultado de la resta es: %d\n", restar(a,b));
+    printf("Resultado de la multiplicacion es: %d\n", multiplicar(a,b));
+    printf("Resultado de la division es: %d\n", division(a,b));
     return 0;
 }
 //4to commit gcc -o runprograma .\tareaenclase.c .\sumar.c .\restar.c .\multiplicar.c .\division.c   >>   se agrego el .exe//
